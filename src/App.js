f5dd475861dc21781c3c0a7dc66b8cda9946596e
Adrian Jurisic <adrianjurisic1601@gmail.com>
2025-07-21 14:51:17 +0200

    Configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl text-white bg-green-500 p-6">
+      Tailwind konačno radi!
     </div>
   );
 }
+
 
 export default App;
